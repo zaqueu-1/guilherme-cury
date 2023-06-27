7ee4d3bcd850ec6fb5af8f1d465179acc26a2edc
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-06-27 19:18:59 -0300

    link update

--- pages/components/Links.jsx
+++ pages/components/Links.jsx
@@ -8,9 +8,15 @@ function Links() {
           <Image className='img' src='/images/gui1.png' width='505' height='205' />
         </a>
 
-        <a href="https://www.instagram.com/stories/highlights/17883684542536997/" className='link' target="_blank" rel="noreferrer">
+        <a href="https://www.instagram.com/s/aGlnaGxpZ2h0OjE3ODgzNjg0NTQyNTM2OTk3?igshid=MTc4MmM1YmI2Ng==" className='link' >
         <Image className='img' src='/images/gui2.png' width='505' height='205' />
         </a>
+
+      <div className="parceiros">
+        <Image className='img2' src='/images/growth.png' width='300' height='200' />
+        <Image className='img2' src='/images/oficialfarma.png' width='300' height='200' />
+      </div>
+
     </div>
   )
 }
